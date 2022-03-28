441ea593eb93a6954deae06cf26e2523b83b4450
tak001 <twitwitwi2@yahoo.co.jp>
2022-03-28 23:35:33 +0900

    feat:blog

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+import Layout from "../components/Layout";
+export default function Home() {
+  return (
+    <Layout title="Home">
+      <p className="text-4xl">Welcome to Nextjs</p>
+    </Layout>
+  );
+}
